877ae1a54a14b8b8664fcb4b8c7067f9f1bc2bce
Shahzad Ansari <shahzadansari03@outlook.com>
2020-09-10 00:42:11 +0500

    Add ProgressBar

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.news;
 import android.app.LoaderManager;
 import android.content.Loader;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -18,20 +20,22 @@ public class MainActivity extends AppCompatActivity
 
     private RecyclerView recyclerView;
     private NewsItemAdapter adapter;
-    private List<NewsItem> newsItemList = new ArrayList<>();
     public static final int NEWS_LOADER_ID = 6;
     public static final String REQUEST_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=c2194f57d73e4392ae4ee0bf69e9d391";
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = findViewById(R.id.progress_circular);
+
         recyclerView = findViewById(R.id.recycler_view);
 
-//        adapter = new NewsItemAdapter(this, new ArrayList<NewsItem>());
-//
-//        recyclerView.setAdapter(adapter);
+        adapter = new NewsItemAdapter(this, new ArrayList<NewsItem>());
+
+        recyclerView.setAdapter(adapter);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager
                 (this, LinearLayoutManager.VERTICAL, false);
@@ -51,6 +55,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onLoadFinished(Loader<List<NewsItem>> loader, List<NewsItem> newsItems) {
 
+        progressBar.setVisibility(View.GONE);
+
         if (newsItems != null && !newsItems.isEmpty()) {
             adapter = new NewsItemAdapter(this, newsItems);
             recyclerView.setAdapter(adapter);
@@ -59,5 +65,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onLoaderReset(Loader<List<NewsItem>> loader) {
+        adapter = null;
+        recyclerView.removeAllViews();
     }
 }
