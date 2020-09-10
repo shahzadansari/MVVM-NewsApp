278a1541a975d43cdf3f0f40622917fecc81d13f
Shahzad Ansari <shahzadansari03@outlook.com>
2020-09-11 02:40:39 +0500

    Add Keyword Search feature

--- app/src/main/java/com/example/news/NewsAPI.java
+++ app/src/main/java/com/example/news/NewsAPI.java
@@ -4,9 +4,15 @@ import com.example.news.models.RootJsonData;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface NewsAPI {
 
     @GET("top-headlines?country=us&apiKey=c2194f57d73e4392ae4ee0bf69e9d391")
-    Call<RootJsonData> getRootJsonData();
+    Call<RootJsonData> getTopHeadlines();
+
+    @GET("everything")
+    Call<RootJsonData> getEverythingFromKeyword(@Query("q") String keyword,
+                                               @Query("apiKey") String apiKey,
+                                               @Query("sortBy") String sortBy);
 }
