562f40a9a711a1d103cb787aecd62556dbc844a7
Shahzad Ansari <shahzadansari03@outlook.com>
2020-09-14 01:21:59 +0500

    Create NewsDetailActivity.java

---
+++ app/src/main/java/com/example/news/NewsDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.news;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newsItem.R;
+
+public class NewsDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detail);
+    }
+}
