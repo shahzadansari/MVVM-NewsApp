241fcca93bc6960fff58f5a99a89fdd0c6d5da85
Shahzad Ansari <shahzadansari03@outlook.com>
2020-12-19 20:30:01 +0500

    Migrate code to NewsFragment

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,64 +1,27 @@
 package com.example.news;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.core.view.MenuItemCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import com.example.news.adapters.NewsItemAdapter;
-import com.example.news.api.NewsAPI;
-import com.example.news.models.NewsItem;
-import com.example.news.models.RootJsonData;
 import com.example.newsItem.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private NewsItemAdapter adapter;
-    private ProgressBar progressBar;
-    private TextView emptyStateTextView;
-    private TextView textViewTitle;
-    private Context mContext;
-    private String keyword = "";
-    private SwipeRefreshLayout swipeRefreshLayout;
-    public static final String SORT_ORDER = "publishedAt";
-    private String language = "";
-    private String locale = "";
 
-    private boolean isLanguageAvailable = false;
-    private boolean isLocaleAvailable = false;
     private BottomNavigationView bottomNavigationView;
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, new NewsFragment())
+                .setReorderingAllowed(true)
+                .commit();
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
@@ -98,175 +61,65 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
-        locale = Utils.getCountry();
-        isLocaleAvailable = Utils.checkLocale(locale);
 
-        language = Locale.getDefault().getLanguage();
-        isLanguageAvailable = Utils.checkLanguage(language);
+    }
 
-//        mContext = this;
-//        progressBar = findViewById(R.id.progress_circular);
-//        emptyStateTextView = findViewById(R.id.empty_view);
-//        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
-//        textViewTitle = findViewById(R.id.text_view_top_headlines);
+
+
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putString("keyword", keyword);
+//    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater menuInflater = getMenuInflater();
+//        menuInflater.inflate(R.menu.menu_main, menu);
 //
-//        if (savedInstanceState != null) {
-//            keyword = savedInstanceState.getString("keyword");
-//        }
+//        searchKeywordFromSearchView(menu);
 //
-//        initEmptyRecyclerView();
-//        fetchData(keyword);
-//        swipeRefreshLayout.setOnRefreshListener(() -> fetchData(keyword));
-    }
+//        return true;
+//    }
 
-    public void initEmptyRecyclerView() {
-
-//        recyclerView = findViewById(R.id.recycler_view);
-        adapter = new NewsItemAdapter(mContext, new ArrayList<NewsItem>(), this);
-        recyclerView.setAdapter(adapter);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager
-                (this, LinearLayoutManager.VERTICAL, false);
-
-        recyclerView.setLayoutManager(linearLayoutManager);
-    }
-
-    public void fetchData(String keyword) {
-        ConnectivityManager connectivityManager = (ConnectivityManager)
-                getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-
-        if (networkInfo != null && networkInfo.isConnected()) {
-
-            Call<RootJsonData> rootJsonDataCall = createJsonDataCall(keyword);
-            rootJsonDataCall.enqueue(new Callback<RootJsonData>() {
-                @Override
-                public void onResponse(Call<RootJsonData> call, Response<RootJsonData> response) {
-                    textViewTitle.setVisibility(View.VISIBLE);
-                    swipeRefreshLayout.setRefreshing(false);
-                    initRecyclerViewWithResponseData(response);
-                }
-
-                @Override
-                public void onFailure(Call<RootJsonData> call, Throwable t) {
-                    textViewTitle.setVisibility(View.INVISIBLE);
-                    swipeRefreshLayout.setRefreshing(false);
-                    emptyStateTextView.setText(t.getMessage());
-                }
-            });
-
-        } else {
-            progressBar.setVisibility(View.GONE);
-            textViewTitle.setVisibility(View.GONE);
-            emptyStateTextView.setText(R.string.no_internet_connection);
-        }
-
-    }
-
-    public Call<RootJsonData> createJsonDataCall(String keyword) {
-
-        NewsAPI newsAPI = ServiceGenerator.createService(NewsAPI.class);
-
-        Call<RootJsonData> rootJsonDataCall;
-
-        if (keyword.isEmpty()) {
-
-            if (isLocaleAvailable) {
-                rootJsonDataCall = newsAPI.getTopHeadlinesByCountry(locale, language, getString(R.string.API_KEY));
-            } else {
-                if (isLanguageAvailable) {
-                    language = Utils.getLanguage();
-                } else {
-                    language = "en";
-                }
-                rootJsonDataCall = newsAPI.getTopHeadlinesByLanguage(language, getString(R.string.API_KEY));
-
-            }
-        } else {
-            rootJsonDataCall = newsAPI.searchArticlesByKeyWord(keyword, SORT_ORDER, language, getString(R.string.API_KEY));
-        }
-
-        return rootJsonDataCall;
-    }
-
-    public void initRecyclerViewWithResponseData(Response<RootJsonData> response) {
-
-        RootJsonData rootJsonData = response.body();
-        List<NewsItem> newsItemList = rootJsonData.getNewsItems();
-
-        progressBar.setVisibility(View.GONE);
-        if (newsItemList.isEmpty()) {
-            emptyStateTextView.setText(R.string.no_news_found);
-        }
-
-        if (!newsItemList.isEmpty()) {
-            adapter = new NewsItemAdapter(mContext, newsItemList, this);
-            recyclerView.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
-        }
-    }
-
-    public void searchKeyword(String query) {
-        initEmptyRecyclerView();
-        progressBar.setVisibility(View.VISIBLE);
-        keyword = query;
-        fetchData(keyword);
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("keyword", keyword);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_main, menu);
-
-        searchKeywordFromSearchView(menu);
-
-        return true;
-    }
-
-    private void searchKeywordFromSearchView(Menu menu) {
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
-
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setQueryHint("Search Latest News...");
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                if (query.length() > 2) {
-                    searchKeyword(query);
-                } else {
-                    Toast.makeText(MainActivity.this, "Type more than two letters!", Toast.LENGTH_SHORT).show();
-                }
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-        MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
-                return true;
-            }
-
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                keyword = "";
-                return true;
-            }
-        });
-
-        searchMenuItem.getIcon().setVisible(false, false);
-    }
+//    private void searchKeywordFromSearchView(Menu menu) {
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+//        MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+//
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        searchView.setQueryHint("Search Latest News...");
+//
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                if (query.length() > 2) {
+//                    searchKeyword(query);
+//                } else {
+//                    Toast.makeText(MainActivity.this, "Type more than two letters!", Toast.LENGTH_SHORT).show();
+//                }
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return false;
+//            }
+//        });
+//
+//        MenuItemCompat.setOnActionExpandListener(searchMenuItem, new MenuItemCompat.OnActionExpandListener() {
+//            @Override
+//            public boolean onMenuItemActionExpand(MenuItem item) {
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onMenuItemActionCollapse(MenuItem item) {
+//                keyword = "";
+//                return true;
+//            }
+//        });
+//
+//        searchMenuItem.getIcon().setVisible(false, false);
+//    }
 }
--- app/src/main/java/com/example/news/NewsFragment.java
+++ app/src/main/java/com/example/news/NewsFragment.java
@@ -1,15 +1,49 @@
 package com.example.news;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.example.news.adapters.NewsItemAdapter;
+import com.example.news.api.NewsAPI;
+import com.example.news.models.NewsItem;
+import com.example.news.models.RootJsonData;
 import com.example.newsItem.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class NewsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private NewsItemAdapter adapter;
+    private ProgressBar progressBar;
+    private TextView emptyStateTextView;
+    private TextView textViewTitle;
+    private Context mContext;
+    private String keyword = "";
+    private SwipeRefreshLayout swipeRefreshLayout;
+    public static final String SORT_ORDER = "publishedAt";
+    private String language = "";
+    private String locale = "";
+    private boolean isLanguageAvailable = false;
+    private boolean isLocaleAvailable = false;
 
     public NewsFragment() {
         // Required empty public constructor
@@ -19,6 +53,121 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_news, container, false);
+
+        locale = Utils.getCountry();
+        isLocaleAvailable = Utils.checkLocale(locale);
+
+        language = Locale.getDefault().getLanguage();
+        isLanguageAvailable = Utils.checkLanguage(language);
+
+        mContext = getActivity();
+        progressBar = rootView.findViewById(R.id.progress_circular);
+        emptyStateTextView = rootView.findViewById(R.id.empty_view);
+        swipeRefreshLayout = rootView.findViewById(R.id.swipe_refresh);
+        textViewTitle = rootView.findViewById(R.id.text_view_top_headlines);
+        recyclerView = rootView.findViewById(R.id.recycler_view);
+
+        if (savedInstanceState != null) {
+            keyword = savedInstanceState.getString("keyword");
+        }
+
+        initEmptyRecyclerView();
+        fetchData(keyword);
+        swipeRefreshLayout.setOnRefreshListener(() -> fetchData(keyword));
+        return rootView;
+    }
+
+    public void initEmptyRecyclerView() {
+
+        adapter = new NewsItemAdapter(mContext, new ArrayList<NewsItem>(), (MainActivity) getActivity());
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager
+                (mContext, LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    public void fetchData(String keyword) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+            Call<RootJsonData> rootJsonDataCall = createJsonDataCall(keyword);
+            rootJsonDataCall.enqueue(new Callback<RootJsonData>() {
+                @Override
+                public void onResponse(Call<RootJsonData> call, Response<RootJsonData> response) {
+                    textViewTitle.setVisibility(View.VISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+                    initRecyclerViewWithResponseData(response);
+                }
+
+                @Override
+                public void onFailure(Call<RootJsonData> call, Throwable t) {
+                    textViewTitle.setVisibility(View.INVISIBLE);
+                    swipeRefreshLayout.setRefreshing(false);
+                    emptyStateTextView.setText(t.getMessage());
+                }
+            });
+
+        } else {
+            progressBar.setVisibility(View.GONE);
+            textViewTitle.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
+    }
+
+    public Call<RootJsonData> createJsonDataCall(String keyword) {
+
+        NewsAPI newsAPI = ServiceGenerator.createService(NewsAPI.class);
+
+        Call<RootJsonData> rootJsonDataCall;
+
+        if (keyword.isEmpty()) {
+
+            if (isLocaleAvailable) {
+                rootJsonDataCall = newsAPI.getTopHeadlinesByCountry(locale, language, getString(R.string.API_KEY));
+            } else {
+                if (isLanguageAvailable) {
+                    language = Utils.getLanguage();
+                } else {
+                    language = "en";
+                }
+                rootJsonDataCall = newsAPI.getTopHeadlinesByLanguage(language, getString(R.string.API_KEY));
+
+            }
+        } else {
+            rootJsonDataCall = newsAPI.searchArticlesByKeyWord(keyword, SORT_ORDER, language, getString(R.string.API_KEY));
+        }
+
+        return rootJsonDataCall;
+    }
+
+    public void initRecyclerViewWithResponseData(Response<RootJsonData> response) {
+
+        RootJsonData rootJsonData = response.body();
+        List<NewsItem> newsItemList = rootJsonData.getNewsItems();
+
+        progressBar.setVisibility(View.GONE);
+        if (newsItemList.isEmpty()) {
+            emptyStateTextView.setText(R.string.no_news_found);
+        }
+
+        if (!newsItemList.isEmpty()) {
+            adapter = new NewsItemAdapter(mContext, newsItemList, (MainActivity) getActivity());
+            recyclerView.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public void searchKeyword(String query) {
+        initEmptyRecyclerView();
+        progressBar.setVisibility(View.VISIBLE);
+        keyword = query;
+        fetchData(keyword);
     }
 }
